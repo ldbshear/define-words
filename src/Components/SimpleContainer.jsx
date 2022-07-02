cb47c8dbebcc2f6a2f603e1b7012b8a1a987423b
Ldbshear-jones <ldbshearjones@gmail.com>
2022-07-02 17:21:13 -0400

    Update SimpleContainer.jsx

--- src/Components/SimpleContainer.jsx
+++ src/Components/SimpleContainer.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-// import Box from "@mui/material/Box";
+
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Title from "./Title";
@@ -12,13 +12,11 @@ export default function SimpleContainer() {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="sm">
-        {/* <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} /> */}
         <Box component="span">
           <Title />
         </Box>
         <InputForm />
       </Container>
-      
     </React.Fragment>
   );
 }
